a5cf329061f4ca927f96ac144159b1005d291e0f
Shibin Vettem Veettil <53273782+shibinnarayanan@users.noreply.github.com>
2022-10-10 20:27:45 +0100

    Add files via upload

---
+++ FutureAsyncTask/src/com/svv/completabletask/async/Async.java
@@ -0,0 +1,28 @@
+package com.svv.completabletask.async;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Async {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		ExecutorService executerServ = Executors.newFixedThreadPool(10);
+
+		Future<List<Integer>> future = executerServ.submit(() -> {
+			
+			System.out.println("Task Submited");
+			Thread.sleep(5000);
+			return Arrays.asList(1, 2, 3, 4);
+		});
+		
+		List<Integer> futureResult = future.get();
+		
+		System.out.println(futureResult);// result will print after 5 sec delay
+	}
+
+}
